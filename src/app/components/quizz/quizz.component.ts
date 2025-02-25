f5889b16ee78450fee183d1b8a814bb8bae1fee9
matheusmgs22 <matheusmgs17@gmail.com>
2025-02-25 11:24:21 -0300

    Criando as propriedades lógicas do projeto

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit{
+  title:string = ""
 
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answersSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
